33538c4dc0314001825feb2c870053b5fe61850f
Muhamedelsayed <42821760+Muhamedelsayed@users.noreply.github.com>
2021-01-25 08:27:44 +0200

    Add files via upload

--- MoSeka/__init__.py
+++ MoSeka/__init__.py
@@ -15,5 +15,5 @@ from Pooling_util import *
 from RBF_initial_weight import *
 from LayerObjects import *
 
-__all__ = ["zero_pad","load_data","sigmoid","sigmoid_prime","tanh","tanh_prime","hard_tanh","hard_tanh_prime","relu","relu_prime","leaky_relu","leaky_relu_prime","Sigmoid","tanh","ReLU","Leaky_ReLU","Softmax","Loss",
+__all__ = ["zero_pad","load_data","sigmoid","sigmoid_prime","tanh","tanh_prime","hard_tanh","hard_tanh_prime","relu","relu_prime","leaky_relu","leaky_relu_prime","Sigmoid","tanh","ReLU","LeakyReLU","Softmax","Loss",
            "MeanSquareLoss","CrossEntropyLoss","Function","Layer","Flatten","Linear","Net"]
