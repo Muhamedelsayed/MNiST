12aba3bbe4779acaa4f725d8dda7674a0d77f980
Muhamedelsayed <42821760+Muhamedelsayed@users.noreply.github.com>
2021-01-25 04:29:30 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'MoSek',         # How you named your package folder (MyLib)
-  packages = ['MoSek'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  name = 'MoSeka',         # How you named your package folder (MyLib)
+  packages = ['MoSeka'],   # Chose the same as "name"
+  version = '1.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Deep learning framework',   # Give a short description about your library
   author = 'Mohamed elsayed',                   # Type in your name
   author_email = 'mohamedelsayed16397@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Muhamedelsayed/MNiST',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Muhamedelsayed/MNiST/archive/V1.0.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Muhamedelsayed/MNiST/archive/V1.6.tar.gz',    # I explain this later on
   keywords = ['Lenet5', 'Framework', 'MNIST'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
