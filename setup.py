e61ef342884553880d1d668d911a6bd72d7a5c67
Muhamedelsayed <42821760+Muhamedelsayed@users.noreply.github.com>
2021-01-25 04:47:28 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'MoSeka',         # How you named your package folder (MyLib)
   packages = ['MoSeka'],   # Chose the same as "name"
-  version = '1.6',      # Start with a small number and increase it with every change you make
+  version = '1.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Deep learning framework',   # Give a short description about your library
   author = 'Mohamed elsayed',                   # Type in your name
   author_email = 'mohamedelsayed16397@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/Muhamedelsayed/MNiST',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Muhamedelsayed/MNiST/archive/V1.6.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Muhamedelsayed/MNiST/archive/V1.7.tar.gz',    # I explain this later on
   keywords = ['Lenet5', 'Framework', 'MNIST'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
@@ -24,7 +24,6 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
+
   ],
 )
