3d676374e6706938e4faff2856df85c50ea3bb27
Muhamedelsayed <42821760+Muhamedelsayed@users.noreply.github.com>
2021-01-25 09:14:47 +0200

    Add files via upload

--- MoSeka/__init__.py
+++ MoSeka/__init__.py
@@ -15,4 +15,4 @@ from Pooling_util import *
 from RBF_initial_weight import *
 from LayerObjects import *
 
-__all__=["load_data","ReLU","Linear","Net","CrossEntropyLoss","micro_F1_SCORE","hot_form","f1_score_labels","macro_f1_score","confusion_matrix","visualise_confusion_for_mnist"]
+__all__=["load_data","ReLU","Linear","Net","micro_F1_SCORE","hot_form","f1_score_labels","macro_f1_score","confusion_matrix","visualise_confusion_for_mnist"]
