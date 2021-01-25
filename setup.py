f7fa64e109b3a0a4ed4e94a339fc6fb65f4a8b50
Muhamedelsayed <42821760+Muhamedelsayed@users.noreply.github.com>
2021-01-25 09:01:16 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 setup_args = dict(
 name = 'MoSeka',  # How you named your package folder (MyLib)
-version = '2.6',  # Start with a small number and increase it with every change you make
+version = '2.7',  # Start with a small number and increase it with every change you make
 license = 'MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
 description = 'Deep learning framework',  # Give a short description about your library
 author = 'Mohamed elsayed',  # Type in your name
 packages=find_packages(),
 author_email = 'mohamedelsayed16397@gmail.com',  # Type in your E-Mail
 url = 'https://github.com/Muhamedelsayed/MNiST',  # Provide either the link to your github or to your website
-download_url = 'https://github.com/Muhamedelsayed/MNiST/archive/2.6.tar.gz',  # I explain this later on
+download_url = 'https://github.com/Muhamedelsayed/MNiST/archive/2.7.tar.gz',  # I explain this later on
 keywords = ['Lenet5', 'Framework', 'MNIST'],  # Keywords that define your package best
 include_package_data=True
 )
