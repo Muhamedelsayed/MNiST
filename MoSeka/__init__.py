ab68866bad1805eb15306e43139f765b79ba63f8
Muhamedelsayed <42821760+Muhamedelsayed@users.noreply.github.com>
2021-01-25 04:45:53 +0200

    Add files via upload

--- MoSeka/__init__.py
+++ MoSeka/__init__.py
@@ -3,7 +3,7 @@ from losses import*
 from layers import*
 from functional import*
 from evaluation_matrix import*
-from activations import*
+from activations import ReLU
 from Utils import*
 from RBF_initial_weight import*
 
